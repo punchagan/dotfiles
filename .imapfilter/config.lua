a80349d22b05c7e702b8af9524bb6193a57392f6
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-10-26 21:13:14 +0530

    More elaborate imapfilter rules.

--- .imapfilter/config.lua
+++ .imapfilter/config.lua
@@ -56,8 +56,12 @@ blaggregator:move_messages(account["Blaggregator"])
 local keras = gh * account.INBOX:contain_to('keras')
 keras:move_messages(account["GitHub"])
 
+gh = account.INBOX:contain_from('notifications@github.com')
+gh:move_messages(account["GitHub"])
+
 -- Lafoots
-local lafoots = account.INBOX:contain_to('lafoots-252@googlegroups.com')
+local lafoots = account.INBOX:contain_to('lafoots-252@googlegroups.com') +
+  account.INBOX:contain_from('park@muse-amuse.in')
 lafoots:move_messages(account["Lafoots"])
 
 -- News/To-Read
@@ -65,3 +69,43 @@ local news = account.INBOX:contain_from('noreply@medium.com') +
   account.INBOX:contain_from('hi@qz.com') +
   account.INBOX:contain_from('support@instapaper.com')
 news:move_messages(account["News"])
+
+-- HackerSchool
+local rc = account.INBOX:contain_subject('[community - ')
+rc:move_messages(account["HackerSchool"])
+
+-- Bitbucket
+local bb = account.INBOX:contain_from('notifications-noreply@bitbucket.org')
+bb:move_messages(account["Bitbucket"])
+
+-- PyCon IN
+local py_in = account.INBOX:contain_from('@pssi.org.in')
+py_in:move_messages(account["PyConIN"])
+
+-- Junk
+local junk = account.INBOX:contain_from('noreply@coursera.org') +
+  account.INBOX:contain_from('nanowrimo.org') +
+  account.INBOX:contain_from('gun.io') +
+  account.INBOX:contain_from('info@twitter.com') +
+  account.INBOX:contain_from('notify@twitter.com') +
+  account.INBOX:contain_from('info@e.twitter.com') +
+  account.INBOX:contain_from('iglaw.com') +
+  account.INBOX:contain_from('@ello.co') +
+  account.INBOX:contain_from('noreply@github.com') +
+  account.INBOX:contain_from('news@edx.org') +
+  account.INBOX:contain_from('information@gandi.net') +
+  account.INBOX:contain_from('info@makersasylum.com') +
+  account.INBOX:contain_from('trello.com') +
+  account.INBOX:contain_from('change.org')
+
+junk:move_messages(account["Junk"])
+
+-- Spam
+local spam = account.INBOX:contain_body('If you no longer wish to receive mail from this sender') +
+  account.INBOX:contain_from('events@qz.com') +
+  ((account.INBOX:contain_body('deals') + account.INBOX:contain_body('sale'))
+      * account.INBOX:contain_body('unsubscribe')) +
+  account.INBOX:contain_from('@etailingindiaexpo')
+
+
+spam:move_messages(account["Trash"])
