8ae6af491412f2d9bd2497788b21dcf47f864607
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-12-19 11:58:43 +0530

    Add script to get readability content for a url.

---
+++ bin/get_article.py
@@ -0,0 +1,29 @@
+#!/home/punchagan/.virtualenvs/hs/bin/python
+from __future__ import print_function
+import requests
+from readability.readability import Document
+
+def get_content(url):
+    try:
+        response = requests.get(url, timeout=10)
+
+    except requests.ConnectionError:
+        print('Error connecting to {}'.format(url))
+
+    else:
+        # Only store the content if the page load was successful
+        if response.ok:
+            page_content = Document(response.content).summary()
+            print(page_content)
+
+        else:
+            print('Error processing URL({}): {}'.format(url, response.reason))
+
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 2:
+        sys.exit('Usage: {} <URL>'.format(sys.argv[0]))
+    url = sys.argv[1]
+    get_content(url)
