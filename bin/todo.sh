edd0444eeb05f902e4d546590846df6b967f0a74
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-04-25 17:33:32 +0530

    Remove unused todo.sh

--- bin/todo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-emacsclient -cne -F '((title . "Capturing TODO") (user-position . t) (left . (+ 550)) (top . (+ 400)) (width . 120) (height . 40))' '(progn (org-capture nil "t") (spacemacs/toggle-maximize-buffer) (recenter 0) (raise-frame) (x-focus-frame (selected-frame)))'
