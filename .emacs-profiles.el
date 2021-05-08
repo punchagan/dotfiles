9f442437fd6dc37d75badbb2081f986af9154a45
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-05-08 14:47:40 +0530

    Use my only hand grown dot files as the default profile

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,4 @@
-(("spacemacs" . ((user-emacs-directory . "~/software/random/spacemacs")))
- ("default" . ((user-emacs-directory . "~/software/random/doom-emacs"))))
+(("default" . ((user-emacs-directory . "~/software/my-repos/my-dot-emacs")))
+ ("purcell" . ((user-emacs-directory . "~/software/my-repos/dot-emacs")))
+ ("spacemacs" . ((user-emacs-directory . "~/software/random/spacemacs")))
+ ("doom" . ((user-emacs-directory . "~/software/random/doom-emacs"))))
